66cc608dbb209b42182b8b27ca665e7229d1ed4d
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2021-02-03 23:30:35 +0900

    add binding

--- app/src/main/java/com/github/sasakitomohiro/indicatorsample/MainActivity.kt
+++ app/src/main/java/com/github/sasakitomohiro/indicatorsample/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.github.sasakitomohiro.indicatorsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.github.sasakitomohiro.indicatorsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
